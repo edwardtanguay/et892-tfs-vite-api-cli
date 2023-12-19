2bf01b2690b99ebaafa145d17081f744b9df203c
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-19 22:39:33 +0100

    backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { IAppData } from "./interfaces";
-import * as tools from './tools';
+import * as tools from "./tools";
+import * as config from "../backend/config";
 
-const backendUrl = "http://localhost:4009/appData";
-const test = 'abc 8273 ksdkfj sdkfj';
+const backendUrl = `http://localhost:${config.backendPort()}/appData`;
+const test = "abc 8273 ksdkfj sdkfj";
 
 function App() {
 	const [appData, setAppData] = useState<IAppData>({} as IAppData);
@@ -22,25 +23,31 @@ function App() {
 			<h1>Info Site</h1>
 			<h2>App Data</h2>
 			<table>
-				<tr>
-					<td>app idCode:</td>{" "}
-					<td className="data">{appData.appIdCode}</td>
-				</tr>
-				<tr>
-					<td>frontend port</td>{" "}
-					<td className="data">{appData.frontendPort}</td>
-				</tr>
-				<tr>
-					<td>backend port</td>{" "}
-					<td className="data">{appData.backendPort}</td>
-				</tr>
+				<tbody>
+					<tr>
+						<td>app idCode:</td>
+						<td className="data">{appData.appIdCode}</td>
+					</tr>
+					<tr>
+						<td>frontend port</td>
+						<td className="data">{appData.frontendPort}</td>
+					</tr>
+					<tr>
+						<td>backend port</td>
+						<td className="data">{appData.backendPort}</td>
+					</tr>
+				</tbody>
 			</table>
 			<h2>Shared Tools</h2>
 			<ul>
-			<li>string with number: <code>{test}</code></li>
-			<li>number extracted: <code>{tools.extractPortNumber(test)}</code></li>
+				<li>
+					string with number: <code>{test}</code>
+				</li>
+				<li>
+					number extracted:{" "}
+					<code>{tools.extractPortNumber(test)}</code>
+				</li>
 			</ul>
-
 		</>
 	);
 }
